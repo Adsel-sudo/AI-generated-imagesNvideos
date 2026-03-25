ce3bba03a83a071b30df8aca68250ce6e113f511
AdeslAIRelatedDevelopment <cutebone000@gmail.com>
2026-03-25 16:41:48 +0800

    优化左侧栏标题与参考图片区文案层级

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -2,6 +2,6 @@ from .enums import TaskStatus
 
 DEFAULT_PROVIDER = "google_image"
 DEFAULT_TASK_TYPE = "image"
-DEFAULT_N_OUTPUTS = 1
+DEFAULT_N_OUTPUTS = 3
 
 STATUS_FINAL = {TaskStatus.DONE.value, TaskStatus.FAILED.value}
